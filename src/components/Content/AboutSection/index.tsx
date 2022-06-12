b02f3bc6416659370924d812290dd5cf4e3d8f54
Gustavo Teixeira <echo.gutotfre@protonmail.com>
2022-06-12 18:11:59 -0300

    Corrigindo erros de tipagem

--- src/components/Content/AboutSection/index.tsx
+++ src/components/Content/AboutSection/index.tsx
@@ -7,7 +7,7 @@ export function AboutSection() {
             <Slide bottom cascade ssrFadeout>
             <div className={styles.aboutMeContainer}>
                 <div className={styles.aboutMeLead}>
-                    <h2>Greatings, it's Me!</h2>
+                    <h2>Greetings, it's Me!</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusantium vero omnis. Dolorum placeat voluptatem nobis esse facere magnam cupiditate harum doloribus neque, minima, mollitia, doloremque fugit nesciunt autem corporis.</p>
                     <a href="#" target="_blank">See more &raquo;</a>
                 </div>
